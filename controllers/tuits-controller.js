e58d1d6628a8ebb160e0e95832740bb37d688392
dsilva-d <dsilva.d@northeastern.edu>
2022-04-15 22:38:38 -0400

    a9

--- controllers/tuits-controller.js
+++ controllers/tuits-controller.js
@@ -32,8 +32,8 @@ const findAllTuits = async (req, res) => {
   res.json(tuits);
 }
 
-const updateTuit = async (req, res) => {
- const tuitdIdToUpdate = req.params.tid;
+const updateTuit = (req, res) => {
+ const tuitdIdToUpdate = req.params['tid'];
  const updatedTuit = req.body;
  const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updatedTuit);
  //tuits = tuits.map(t => t._id === tuitdIdToUpdate ? updatedTuit : t);
